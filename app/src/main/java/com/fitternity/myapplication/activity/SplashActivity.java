9fbc3a12b2dbb6084042c7c3c25e1c507b34ef23
Pooja <poojap@appxerbia.com>
2021-03-27 19:41:47 +0530

    Date27-3-21

--- app/src/main/java/com/fitternity/myapplication/activity/SplashActivity.java
+++ app/src/main/java/com/fitternity/myapplication/activity/SplashActivity.java
@@ -1,14 +1,16 @@
-package com.fitternity.myapplication;
+package com.fitternity.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.fitternity.myapplication.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        setContentView( R.layout.activity_splash );
     }
 }
